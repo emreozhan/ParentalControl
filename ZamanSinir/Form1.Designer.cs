43ce48180278fee9b38c862bcd6d848dda1124d6
H.Emre Özhan <emreozhan71@gmail.com>
2017-06-30 22:54:51 +0300

    First Commit

---
+++ ZamanSinir/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Win
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.appHizala = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.logUpdate = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Oturum Aç - Devam ET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // appHizala
+            // 
+            this.appHizala.Tick += new System.EventHandler(this.appHizala_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 146);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(232, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kullanici Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Şifre";
+            // 
+            // logUpdate
+            // 
+            this.logUpdate.Tick += new System.EventHandler(this.logUpdate_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Kill";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(141, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kalan Zaman:";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(144, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Duraklat (Pause)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 710);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1366, 768);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1364, 726);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer appHizala;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer logUpdate;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ZamanSinir/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Win
+{
+    public partial class Form1 : Form
+    {
+        static int DKSINIRI=180;
+        int TODAY=0;
+        int MONTH = 0;
+        int YEAR= 0;
+        int HOUR=0;
+        int MIN = 0;
+
+        string ACTIVEUSER=null;
+        string ACTIVEPW = null;
+        string dosya_yolu = null;
+        int ToplamSure = 0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ShowInTaskbar = false;
+            System.Diagnostics.Process.Start("shutdown", "-s -t 300");
+
+            DateTime UTCNow = DateTime.UtcNow;
+            TODAY = UTCNow.Day;
+            MONTH = UTCNow.Month;
+            YEAR = UTCNow.Year;
+           
+            dosya_yolu = @"E:\EmreZamanLog\" + TODAY.ToString()+"."+ MONTH.ToString()+"."+YEAR.ToString() + ".txt";
+            
+            
+            appHizala.Interval = 3700;//7 Sn
+            logUpdate.Interval = 300 * 1000;//5 Dk 
+            appHizala.Start();
+           
+        }
+        
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                cp.ExStyle |= 0x80;  // Turn on WS_EX_TOOLWINDOW
+                return cp;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            ACTIVEUSER = textBox1.Text.ToLower();
+            ACTIVEPW = textBox2.Text.ToLower();
+            if (ACTIVEUSER.Equals("eren"))
+            {
+                appHizala.Start();
+                logUpdate.Start();
+                LogRead(ACTIVEUSER);
+
+                System.Diagnostics.Process.Start("shutdown", "-a");
+                System.Diagnostics.Process.Start("shutdown", "-s -t " + ((DKSINIRI - ToplamSure)*60).ToString() + "");
+
+            }
+            else if (ACTIVEUSER.Equals("root") && ACTIVEPW.Equals("root"))
+                    {
+                        System.Diagnostics.Process.Start("shutdown", "-a");
+                        appHizala.Stop();
+                        logUpdate.Stop();
+                        TopMost = false;
+                    }
+
+        }
+
+        public void LogWrite()
+        {
+            FileStream fs = new FileStream(dosya_yolu, FileMode.OpenOrCreate, FileAccess.Write);
+            fs.Close();
+            
+            DateTime UTCNow = DateTime.UtcNow;
+
+            HOUR = UTCNow.Hour+3;
+            MIN = UTCNow.Minute;
+
+            File.AppendAllText(@"" + dosya_yolu + "", "" + TODAY.ToString() + ";" + ACTIVEUSER + ";5;"+HOUR.ToString()+":"+MIN.ToString()+ Environment.NewLine);
+            
+
+            LogRead(ACTIVEUSER);
+        }
+
+        public void LogRead(string userName)
+        {
+            FileStream fs = new FileStream(dosya_yolu, FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sw = new StreamReader(fs);
+            string yazi = sw.ReadLine();
+            ToplamSure = 0;
+
+            while (yazi != null)
+            {
+                 
+                string [] parsed = yazi.Split(';');
+                if(parsed[0].Equals(TODAY.ToString()) && parsed[1].Equals(userName))
+                {
+                    ToplamSure += Convert.ToInt32(parsed[2]);
+                }              
+                yazi = sw.ReadLine();
+
+            }
+
+            if (ToplamSure > DKSINIRI)
+            {               
+                label3.Text=("SUREN DOLDU 2.5DK İÇİNDE PC KAPATILACAK");
+                TopMost = true;
+                appHizala.Start();
+                logUpdate.Stop();
+                System.Diagnostics.Process.Start("shutdown", "-a");
+
+                System.Diagnostics.Process.Start("shutdown","-s -t 150");
+            }
+            else
+            {
+                label3.Text = ("Bugünlük Kalan Süren" + (DKSINIRI - ToplamSure).ToString());
+               appHizala.Stop();
+               TopMost = false;
+
+            }
+            sw.Close();
+            fs.Close();
+        }
+
+        private void appHizala_Tick(object sender, EventArgs e)
+        {
+            Shell32.ShellClass objShel = new Shell32.ShellClass();
+            objShel.ToggleDesktop();
+            this.Location = new Point(0, 0);
+        }
+
+        private void logUpdate_Tick(object sender, EventArgs e)
+        {
+            LogWrite();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals("root"))
+            { this.Close();}
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //pause
+            System.Diagnostics.Process.Start("shutdown", "-a");
+            TopMost = true;
+            appHizala.Start();
+            logUpdate.Stop();
+
+           // SendKeys.Send("{LWINDOWN}");
+            //System.Diagnostics.Process.Start(@"C:\WINDOWS\system32\rundll32.exe", "user32.dll,LockWorkStation");
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Alt)
+            {
+                e.SuppressKeyPress = true;
+                label1.Text = "ent";
+            }
+        }
+
+        private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FileStream fs = new FileStream(dosya_yolu, FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sw = new StreamReader(fs);
+            string yazi = sw.ReadLine();
+            int cikisTotal = 0;
+
+            while (yazi != null)
+            {
+
+                string[] parsed = yazi.Split(';');
+                if (parsed[0].Equals(TODAY.ToString()) && parsed[1].Equals("eren"))
+                {
+                    cikisTotal += Convert.ToInt32(parsed[2]);
+                }
+                yazi = sw.ReadLine();
+
+            }
+            fs.Close();
+
+            FileStream fsYaz = new FileStream(dosya_yolu, FileMode.OpenOrCreate, FileAccess.Write);
+            fsYaz.Close();
+            
+            File.AppendAllText(@"" + dosya_yolu + "", "x;x;0;" + cikisTotal.ToString() + Environment.NewLine);
+
+            if (textBox1.Text == "root" && textBox2.Text == "root")
+            {
+                System.Diagnostics.Process.Start("shutdown", "-a");
+            }
+            else{
+                System.Diagnostics.Process.Start("shutdown", "-a");
+                System.Diagnostics.Process.Start("shutdown", "-s -t 30");
+            }
+        }
+
+     
+    }
+}
